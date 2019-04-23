7c491c81e347e1b3f19d947b097c8225a8c9b3c1
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2019-04-23 12:18:10 -0700

    New interaction effects for the sign in button.

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -6,6 +6,11 @@ function Button ({ children, ...rest}) {
 }
 
 const StyledButton = Styled('button')`
+  &:active {
+    box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.5);
+    background: #2b47e5;
+  }
+  &:hover { box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.1); }
   background: #304FFE;
   border: none;
   border-radius: 25px;
@@ -15,6 +20,7 @@ const StyledButton = Styled('button')`
   font-size: 16px;
   font-weight: 500;
   padding: 13px 30px;
+  transition: box-shadow 250ms, background 250ms;
 `
 
 export default Button
